88ec4711a4a2f49f3428511b8f6d0c72c317652b
Gian Delfin <giandelfin@outlook.com>
2021-04-26 20:35:45 -0700

    starting set trie tests

--- engine/core/utils/tests/set_trie_test.cpp
+++ engine/core/utils/tests/set_trie_test.cpp
@@ -1,7 +1,12 @@
+
 #include <gtest/gtest.h>
+#include <string>
+
 #include "../set_trie.h"
 
 TEST(testMath, myCubeTest)
 {
+    SetTrie<uint64_t, std::string> test_set_trie;
+    test_set_trie.InsertValueForKeySet("node 1", { 3, 4, 7 });
     EXPECT_EQ(1000, 1000);
 }
