2d7e23fcde2314bf78253af30cd27e7a7e85aeb8
Gian Delfin <giandelfin@outlook.com>
2021-09-30 12:25:56 -0700

    almost finished with serialization

--- core/serialize/archive.h
+++ core/serialize/archive.h
@@ -19,7 +19,7 @@ public:
     template<typename... Ts>
     void SerializeHumanReadable(std::ostream& xml_ostream, std::pair<std::string, Ts&> ...objects)
     {
-		{ SerializeHumanReadable(xml_ostream, objects.first, objects.second)... };
+		{ SerializeHumanReadableIfAble(xml_ostream, objects.first, objects.second)... };
     }
 
 	template<typename T>
@@ -29,11 +29,11 @@ public:
 
 		// It is assumed that any unresolved pointers point to objects allocated on the heap.
 		xml_ostream << "<Heap>\n";
-		for (std::pair<std::size_t, std::ostream> pair : unresolved_pointer_ostreams_) {
+		for (std::pair<std::size_t, std::ostream> pair : unresolved_pointer_serializations_) {
 			xml_ostream << pair.second;
 		}
 		xml_ostream << "<\Heap>\n";
-		unresolved_pointer_ostreams_.clear();
+		unresolved_pointer_serializations_.clear();
 		id_to_object_map_.clear();
 		object_to_id_map_.clear();
 		return xml_ostream;
@@ -55,7 +55,7 @@ public:
 private:
     std::vector<void*> id_to_object_map_;
     std::unordered_map<void*, std::size_t> object_to_id_map_;
-	std::unordered_map<std::size_t, std::ostream> unresolved_pointer_ostreams_;
+	std::unordered_map<std::size_t, std::ostream> unresolved_pointer_serializations_;
 
 	std::size_t Store(void* object_ptr)
 	{
@@ -73,86 +73,80 @@ private:
 		return 0;
 	}
 
+	// Override for std::shared_ptrs
 	template<typename T>
 	void SerializeHumanReadable(std::ostream& xml_ostream, std::string name, std::shared_ptr<T>& obj_shared_ptr)
 	{
-		std::size_t id = IdForObject((void*)&obj_shared_ptr);
-		if (!id) {
-			id = Store((void*)&obj_shared_ptr);
-		}
-		else {
-			unresolved_pointers_.erase(id);
-		}
 		xml_ostream << "<" << name << " type=SharedPointer id=" + id + ">\n";
-		SerializeHumanReadable("ptr", obj_shared_ptr.get());
-		stream_ << "</" + name + ">\n";
+		SerializeHumanReadableIfAble(xml_ostream, "ptr", obj_shared_ptr.get());
+		xml_ostream << "</" + name + ">\n";
 	}
 
+	// Override for std::vectors
 	template<typename T>
 	void SerializeHumanReadable(std::ostream& xml_ostream, std::string name, std::vector<T>& obj_vec)
 	{
-		std::size_t id = IdForObject((void*)&obj_vec);
-		if (!id) {
-			id = Store((void*)&obj_vec);
-		}
-		else {
-			unresolved_pointers_.erase(id);
-		}
 		xml_ostream << "<" << name << " type=Vector id=" + id + ">\n";
-		xml_ostream << "<Count>" << obj_vec.size() << "</Count>\n";
+		xml_ostream << "<count>" << obj_vec.size() << "</count>\n";
 		for (T& object : obj_vec) {
-			SerializeHumanReadable(xml_ostream, "Element", object);
+			SerializeHumanReadableIfAble(xml_ostream, "element", object);
 		}
 		xml_ostream << "</" + name + ">\n";
 	}
 
 	template<typename T>
+	void SerializeHumanReadable(std::ostream& xml_ostream, std::string name, T*& object)
+	{
+		std::size_t pointee_id = IdForObject((void*)object);
+		if (!pointee_id) {
+			pointee_id = Store((void*)object);
+			// Serialize object being pointed to and . Does not handle the case of a pointer to an array of objects.
+			std::ostream temp_ostream;
+			SerializeHumanReadableIfAble(temp_ostream, std::empty, *object);
+			unresolved_pointer_serializations_.insert({ pointee_id , temp_ostream });
+		}
+		xml_ostream << "<Pointer pointee_id=" << pointee_id << "/>\n";
+	}
+
+	template<typename T>
 	void SerializeHumanReadable(std::ostream& xml_ostream, std::string name, T& object)
+	{
+		ISerializable* object_serializable = dynamic_cast<ISerializable*>(object);
+		if (serializable) {
+			object_serializable->SerializeHumanReadable(this, xml_ostream);
+		}
+		else {
+			xml_ostream << object;
+		}
+	}
+
+	template<typename T>
+	void SerializeHumanReadableIfAble(std::ostream& xml_ostream, std::string name, T& object)
 	{
 		std::size_t id = IdForObject((void*)&object);
 		if (!id) {
 			id = Store((void*)&object);
+			if (name != std::empty) {
+				xml_ostream << "<" << name << " id=" << id << ">\n";
+			}
+			SerializeHumanReadable(xml_ostream, name, object);
 		}
 		else {
-			// We have already visited and serialized this object. This is a cycle. Stop recursing.
-			xml_ostream << "<" << name << " id=" << id << ">\n";
-			xml_ostream << unresolved_pointer_ostreams_[id];
-			xml_ostream << "</" << name << ">\n";
+			std::unordered_map<std::size_t, std::ostream>::iterator iter = unresolved_pointer_serializations_.find(id);
+			if (iter == unresolved_pointer_serializations_.end()) {
+				return;
+			}
 
-			unresolved_pointer_ostreams_.erase(id);
+			// We have already visited and serialized this object. This is a cycle. Stop recursing.
+			if (name != std::empty) {
+				xml_ostream << "<" << name << " id=" << id << ">\n";
+			}
+			xml_ostream << iter->second;
+			unresolved_pointer_serializations_.erase(id);
 			return;
 		}
 
-		if (std::is_pointer<T>::value) {
-			std::size_t pointee_id = IdForObject((void*)object);
-			if (!id) {
-				pointee_id = Store((void*)object);
-				// Serialize object being pointed to. Does not handle the case of a pointer to an array of objects.
-				std::ostream temp_ostream;
-
-				ISerializable* object_serializable = dynamic_cast<ISerializable*>(object);
-				if (serializable) {
-					// TODO: handle scenarios when object is std::vector<>* or std::shared_ptr<>*.  
-					object_serializable->SerializeHumanReadable(this, temp_ostream);
-				}
-				else {
-					temp_ostream << *object;
-				}
-
-				unresolved_pointer_ostreams_.insert({ pointee_id , temp_ostream });
-			}
-			xml_ostream << "<" << name << " type=pointer id=" << id << " pointee_id=" << pointee_id << " />\n";
-		}
-		else {
-			xml_ostream << "<" << name << " id=" << id << ">\n";
-
-			ISerializable* object_serializable = dynamic_cast<ISerializable*>(object);
-			if (serializable) {
-				object_serializable->SerializeHumanReadable(this, xml_ostream);
-			}
-			else {
-				xml_ostream << object;
-			}
+		if (name != std::empty) {
 			xml_ostream << "</" << name << ">\n";
 		}
 	}
